416b89416ac1f3fdc9f5817a8d36177734cf944c
DaFlusher <joyibe5@gmail.com>
2025-02-08 19:24:48 +0100

    testing

--- stage-zero/api/index.js
+++ stage-zero/api/index.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 
 // API Endpoint
-app.get("/", (req, res) => {
+app.get("/api", (req, res) => {
   res.status(200).json({
     email: "joyibe5@gmail.com", 
     current_datetime: new Date().toISOString(), 
