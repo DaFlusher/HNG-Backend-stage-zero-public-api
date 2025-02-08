3c30c96ff9ae8ea093083f71ef1df3fdd54c377d
DaFlusher <joyibe5@gmail.com>
2025-02-08 19:47:15 +0100

    removing app.listen

--- stage-zero/api/index.js
+++ stage-zero/api/index.js
@@ -20,7 +20,7 @@ app.get("/", (req, res) => {
 });
 
 // Start Server
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Server running on http://localhost:${PORT}`);
+// });
 export default app;
